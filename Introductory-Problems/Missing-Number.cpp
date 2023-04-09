373e1212ef66e79262281aa335423d64e199f39b
GN6 <todawernher@gmail.com>
2023-04-08 23:54:21 -0400

    AC

---
+++ Introductory-Problems/Missing-Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    bool found[n];
+    fill(found, found + n, false);
+    for (int i = 1; i < n; i++) {
+        int j;
+        scanf("%d", &j);
+        found[j - 1] = true;
+    }
+    for (int i = 0; i < n; i++)
+        if (!found[i]) printf("%d\n", i + 1);
+
+    return 0;
+}
