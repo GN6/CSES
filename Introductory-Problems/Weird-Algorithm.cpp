2f39bcbb8fcba0bc9483b5c6bd4d6533c9eb55c1
GN6 <todawernher@gmail.com>
2023-04-08 23:32:11 -0400

    AC

---
+++ Introductory-Problems/Weird-Algorithm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld", n);
+    while (n != 1) {
+        if (n % 2) n = n * 3 + 1;
+        else n /= 2;
+        printf(" %lld", n);
+    }
+    printf("\n");
+    return 0;
+}
