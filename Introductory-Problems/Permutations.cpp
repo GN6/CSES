899c43dbc99510f272c3d8e24c4c08181553d319
GN6 <todawernher@gmail.com>
2023-04-09 00:33:50 -0400

    AC

---
+++ Introductory-Problems/Permutations.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n == 1) printf("1");
+    else if (n < 4) printf("NO SOLUTION");
+    else {
+        printf("2");
+        for (int i = 4; i <= n; i += 2)
+            printf(" %d", i);
+        for (int i = 1; i <= n; i += 2)
+            printf(" %d", i);
+    }
+    printf("\n");
+
+    return 0;
+}
