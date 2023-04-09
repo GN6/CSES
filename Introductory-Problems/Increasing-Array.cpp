5713ab18aad7d439c1c5b28b3bd76dd081654ffc
GN6 <todawernher@gmail.com>
2023-04-09 00:18:21 -0400

    AC

---
+++ Introductory-Problems/Increasing-Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    long long x[n];
+    for (int i = 0; i < n; i++)
+        scanf("%lld", &x[i]);
+    
+    long long moves = 0, needed = x[0];
+    for (int i = 1; i < n; i++)
+        moves += max((long long) 0, needed - x[i]), needed = max(needed, x[i]);
+    printf("%lld\n", moves);
+
+    return 0;
+}
