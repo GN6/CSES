768366e82d31c6720cca47053418b5c10849e793
GN6 <todawernher@gmail.com>
2023-04-12 21:50:33 -0400

    AC

---
+++ Introductory-Problems/Coin-Piles.cpp
@@ -0,0 +1,16 @@
+// https://cses.fi/problemset/task/1754
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        printf(((a + b) % 3 == 0 && min(a, b) * 2 >= max(a, b)) ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
